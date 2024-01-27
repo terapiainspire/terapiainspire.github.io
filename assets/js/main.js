1a9cb3fed299342bdbb29848b8141ff8d8364720
Fabio Nobre <fabiohcnobre@hotmail.com>
2024-01-27 20:54:20 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,4 +120,14 @@
 			speed: 1000
 		});
 
+		$('a[href^="#"]').on('click', function(event) {
+			var target = $(this.getAttribute('href'));
+			if( target.length ) {
+				event.preventDefault();
+				$('html, body').stop().animate({
+					scrollTop: target.offset().top - (200 * parseInt($("body").css("line-height")))
+				}, 1000);
+			}
+		});
+
 })(jQuery);
